0a17b442898519101c936ed9d7f8b437670134c8
AnnaLoiko <amromany@gmail.com>
2019-07-25 21:43:00 +0300

    feat: add material-ui, setup basic navigation

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,6 +9,7 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-material-ui`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
--- src/pages/architects.js
+++ src/pages/architects.js
@@ -5,11 +5,8 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const SecondPage = () => (
-  <Layout>
-    <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
+  <Layout title={"Architects"}>
+    <h1>Architects page</h1>
   </Layout>
 )
 
