35c0249e8452b6c20fff512d028a5c05f535ed16
Strafi <anton.solodkof@gmail.com>
2019-07-26 20:08:12 +0300

    edit server config

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,22 +1,15 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
-
-// You can delete this file if you're not using it
+/* eslint consistent-return: 0 */
 const path = require('path');
 
 exports.createPages = ({ actions, graphql }) => {
   const { createPage } = actions;
 
-  const arcPage = path.resolve('src/components/newPage.jsx');
+  const arcPage = path.resolve('src/components/ArchitectPage/architectPage.jsx');
 
   return graphql(`{
     allContentfulArchitect {
       edges {
         node {
-          id
           name
           date
           birthPlace
@@ -24,55 +17,20 @@ exports.createPages = ({ actions, graphql }) => {
           videoId
           vita
           path
-          timelineData {
-            _1 {
-              text
-              date
-            }
-            _2 {
-              text
-              date
-            }
-            _3 {
-              text
-              date
-            }
-            _4 {
-              date
-              text
-            }
-            _5 {
-              date
-              text
-            }
-            _6 {
-              date
-              text
-            }
-            _7 {
-              date
-              text
-            }
-            _8 {
-              date
-              text
-            }
-            _9 {
-              date
-              text
-            }
-            _10 {
-              date
-              text
-            }
-            _11 {
-              date
-              text
-            }
-          }
           img {
             file {
               url
+            }
+          }
+          id
+          timelineData {
+            internal {
+              content
+            }
+          }
+          work {
+            internal {
+              content
             }
           }
         }
@@ -90,6 +48,9 @@ exports.createPages = ({ actions, graphql }) => {
           createPage({
             path: node.path,
             component: arcPage,
+            context: {
+              node,
+            },
           });
         }
       });
