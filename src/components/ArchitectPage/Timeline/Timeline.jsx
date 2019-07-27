a7444fab87ced04b9bc1d86f485678cdcdff9dd5
leonidshparun <leonidshparun@gmail.com>
2019-07-27 23:49:45 +0300

    feat: implement timeline component

--- src/components/ArchitectPage/Timeline/Timeline.jsx
+++ src/components/ArchitectPage/Timeline/Timeline.jsx
@@ -1,7 +1,39 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
 
-import style from './style.module.css';
+import { Timeline, TimelineItem } from 'vertical-timeline-component-for-react';
 
-const timeline = () => <div className={style.Container}>Timeline</div>;
+const useStyles = makeStyles(() => ({
+  root: {
+    padding: '10px',
+    margin: '10px',
+    width: '1000px',
+    textAlign: 'center',
+  },
+}));
+
+const timeline = ({ timelineData }) => {
+  const data = Object.values(JSON.parse(timelineData.internal.content));
+  const classes = useStyles();
+  return (
+    <Paper className={classes.root}>
+      <Typography variant="h4" component="h3">Хронология</Typography>
+      <Timeline lineColor="#ddd">
+        {data.map(event => (
+          <TimelineItem
+            key={event.text.length}
+            dateText={event.date}
+            dateInnerStyle={{ background: '#3f51b5' }}
+            style={{ color: '#3f51b5' }}
+          >
+            <p style={{ fontSize: '20px' }}>{event.text}</p>
+          </TimelineItem>
+        ))}
+      </Timeline>
+    </Paper>
+  );
+};
 
 export default timeline;
