11e65dba218c16de2691c708f4e437f2b845274e
leonidshparun <leonidshparun@gmail.com>
2019-07-27 22:44:08 +0300

    feat: implement person component

--- src/components/ArchitectPage/Person/Person.jsx
+++ src/components/ArchitectPage/Person/Person.jsx
@@ -1,7 +1,55 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
 
-import style from './style.module.css';
+const useStyles = makeStyles(() => ({
+  root: {
+    padding: '10px',
+    margin: '10px',
+    width: '1000px',
+  },
+  image: {
+    minWidth: '360px',
+  },
+  bio: {
+    minWidth: '540px',
+  },
+}));
 
-const person = () => <div className={style.Container}>Person</div>;
+const person = ({
+  img,
+  name,
+  date,
+  vita,
+}) => {
+  const classes = useStyles();
+  return (
+    <Paper className={classes.root}>
+      <Grid
+        container
+        alignItems="center"
+        direction="row"
+        wrap="nowrap"
+      >
+        <Grid item className={classes.image}>
+          <img alt={name} width="320" src={img.file.url} />
+        </Grid>
+        <Grid item className={classes.bio}>
+          <Typography variant="h4" component="h1">
+            {name}
+          </Typography>
+          <Typography variant="h6" component="h3">
+            {date}
+          </Typography>
+          <Typography component="p">
+            {vita}
+          </Typography>
+        </Grid>
+      </Grid>
+    </Paper>
+  );
+};
 
 export default person;
