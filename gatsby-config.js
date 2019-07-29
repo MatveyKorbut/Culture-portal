9be44ec18af4a255e66f219da02a89206f3d82fd
AnnaLoiko <amromany@gmail.com>
2019-07-29 17:23:06 +0300

    feat: add design styles and responsiv version for header and navigation

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@ if (process.env.NODE_ENV === "development") {
 }
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Cultural Portal`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
   },
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,19 +1,144 @@
 import { Link } from 'gatsby';
 import React from 'react';
+
+import clsx from 'clsx';
+import Drawer from '@material-ui/core/Drawer';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import Home from '@material-ui/icons/Home';
+import People from '@material-ui/icons/People';
+import Search from '@material-ui/icons/Search';
+import IconButton from '@material-ui/core/IconButton';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import MenuIcon from '@material-ui/icons/Menu';
+import { makeStyles } from '@material-ui/core/styles';
+
 import style from './style.module.css';
 
-const Nav = () => (
-  <div className={style.main}>
-    <Link to="/">
-      MAIN
-    </Link>
-    <Link to="/search">
-      TO THE LIST OF ARCHITECTS
-    </Link>
-    <Link to="/team">
-      TEAM
-    </Link>
-  </div>
-);
+const useStyles = makeStyles(theme => ({
+  toolbarIcon: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    padding: '0 8px',
+    ...theme.mixins.toolbar,
+    background: '#08a3a0',
+  },
+  toolbarIconHidden: {
+    visibility: 'hidden',
+  },
+  drawerPaper: {
+    whiteSpace: 'nowrap',
+    transition: theme.transitions.create('width', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    display: 'none',
+    [theme.breakpoints.down('sm')]: {
+      display: 'block',
+    },
+  },
+  drawerPaperClose: {
+    overflowX: 'hidden',
+    transition: theme.transitions.create('width', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    width: theme.spacing(7),
+    [theme.breakpoints.up('sm')]: {
+      width: 56,
+    },
+  },
+  iconButton: {
+    color: 'white',
+    display: 'none',
+    [theme.breakpoints.down('sm')]: {
+      display: 'block',
+      margin: 0,
+      padding: '5px 10px',
+    },
+  },
+  responsiveLink: {
+    textDecoration: 'none',
+    color: '#006668',
+  },
+}));
+
+const menuItems = [{
+  text: 'Home',
+  icon: <Home />,
+  link: '/',
+}, {
+  text: 'To the list of architects',
+  icon: <Search />,
+  link: '/search',
+}, {
+  text: 'Team',
+  icon: <People />,
+  link: '/team',
+},
+];
+
+const Nav = () => {
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
+
+  return (
+    <div className={style.main}>
+      <IconButton
+        edge="start"
+        color="inherit"
+        aria-label="open drawer"
+        onClick={() => setOpen(true)}
+        className={clsx(classes.iconButton)}
+      >
+        <MenuIcon />
+      </IconButton>
+
+      <Drawer
+        variant="permanent"
+        open={open}
+        classes={{
+          paper: clsx(classes.drawerPaper, !open && classes.drawerPaperClose),
+        }}
+      >
+
+        <div
+          className={clsx(classes.toolbarIcon, !open && classes.toolbarIconHidden)}
+          onClick={() => setOpen(false)}
+        >
+          <IconButton onClick={() => setOpen(false)}>
+            <ChevronLeftIcon />
+          </IconButton>
+        </div>
+
+        <List>
+          {menuItems.map((item) => {
+            return (
+              <Link to={item.link} className={clsx(classes.responsiveLink)} activeClassName="active">
+                <ListItem button key={item.text}>
+                  <ListItemIcon>{item.icon}</ListItemIcon>
+                  <ListItemText primary={item.text} />
+                </ListItem>
+              </Link>
+            );
+          })
+          }
+        </List>
+      </Drawer>
+
+      <nav className={style.nav}>
+        {menuItems.map((item) => {
+          return (
+            <Link to={item.link} activeClassName="active">{item.text}</Link>
+          );
+        })
+        }
+      </nav>
+    </div>
+  )
+};
 
 export default Nav;
