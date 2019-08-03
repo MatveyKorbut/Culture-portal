99d7d9e2926c0ca7f5879778b25ef601ba8ccb27
leonidshparun <leonidshparun@gmail.com>
2019-08-03 18:54:10 +0300

    feat: add link to github repo

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import IconButton from '@material-ui/core/IconButton';
 import style from './style.module.css';
+import github from '../../images/github.png';
 
 const Footer = () => (
   <footer className={style.main}>
-    <div className={style.container}>Rolling Scopes School, 2019</div>
+    <div className={style.container}>
+      <p className={style.copyright}>
+        Rolling Scopes School, 2019
+      </p>
+    </div>
+
+    <IconButton href="https://github.com/MatveyKorbut/Culture-portal">
+      <img alt="github" width="20" src={github} />
+    </IconButton>
   </footer>
 );
 
